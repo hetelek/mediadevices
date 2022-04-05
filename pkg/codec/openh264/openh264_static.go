d033dd4851fb10177bba63eca6fb0b911923f258
Stergios Hetelekides <hetelek@gmail.com>
2022-04-04 23:39:09 -0700

    build for darwin arm

--- pkg/codec/openh264/openh264_static.go
+++ pkg/codec/openh264/openh264_static.go
@@ -8,5 +8,6 @@ package openh264
 //#cgo linux,arm64 LDFLAGS: ${SRCDIR}/lib/libopenh264-linux-arm64.a
 //#cgo linux,amd64 LDFLAGS: ${SRCDIR}/lib/libopenh264-linux-x64.a
 //#cgo darwin,amd64 LDFLAGS: ${SRCDIR}/lib/libopenh264-darwin-x64.a
+//#cgo darwin,arm LDFLAGS: ${SRCDIR}/lib/libopenh264-darwin-arm.a
 //#cgo windows,amd64 LDFLAGS: ${SRCDIR}/lib/libopenh264-windows-x64.a -lssp
 import "C"
